5dd25c76e391beff73820cfa19a37eba7401d662
sb <sb@Shanes-MacBook-Air.local>
2018-03-31 16:04:00 -0700

    Cloud Vision API implemented

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
+require('dotenv').config()
+var vision = require('@google-cloud/vision');
+client = new vision.ImageAnnotatorClient();
+
 var express = require('express')
 var app = express()
 
 app.use(express.json())
 app.use(express.urlencoded())
 
-app.listen(4000)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT)
+
+app.get('/', function (req, res) {
+  res.send("You're not supposed to be here!")
+})
 
 app.post('/', function (req, res) {
   var sourceImage = req.body.srcs
-  console.log(sourceImage[0])
+
+
+  client.labelDetection(sourceImage[0])
+  .then(function(results) {
+    var labels = results[0].labelAnnotations;
+
+    console.log('Labels:');
+    labels.forEach(label => console.log(label.description));
+  })
+  .catch(function(err) {
+    console.error('ERROR:', err);
+  });
+
+  res.send(sourceImage[0])
+
 
 })
