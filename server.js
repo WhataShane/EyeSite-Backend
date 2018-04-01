5e702637110b11f8bae4cf24660270b892f40e36
sb <sb@Shanes-Air.attlocal.net>
2018-04-01 00:27:41 -0700

    Switched from stringing with then to stringing with async/await

--- server.js
+++ server.js
@@ -18,24 +18,18 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
   var sourceImage = req.body.srcs
-  GoogleCloudAnalysis(sourceImage[0])
-
-  res.send(sourceImage[0])
+  imageAnalysis = await GoogleCloudAnalysis(sourceImage[0])
+  res.send(imageAnalysis)
 })
 
-app.post('/', function (req, res) {
- var sourceImage = req.body.srcs
- imageAnalysis = []
- GoogleCloudAnalysis(sourceImage[0], function(result){
-   res.send(result)
- })
-})
 
-function GoogleCloudAnalysis (sourceImage, callback) {
+
+
+async function GoogleCloudAnalysis (sourceImage) {
 
   var imageAnalysis = []
 
-  client.labelDetection(sourceImage)
+  const result = await client.labelDetection(sourceImage)
   .then(function(results) {
     var labels = results[0].labelAnnotations
     var objects = []
@@ -48,47 +42,43 @@ function GoogleCloudAnalysis (sourceImage, callback) {
     console.error('ERROR:', err)
   })
 
-  .then(function(results){
+  const result2 = await client.faceDetection(sourceImage)
+  .then(function(results) {
+    var faces = results[0].faceAnnotations
+    imageAnalysis.push({numberOfPeople: Object.keys(faces).length})
+    var numberOfHappyPeople = 0
+    var numberOfSadPeople = 0
+    var numberOfSurprisedPeople = 0
+    var numberOfAngeredPeople = 0
+    var facesProcessed = 0;
 
-    client.faceDetection(sourceImage)
-    .then(function(results) {
-      var faces = results[0].faceAnnotations
+    faces.forEach(function(face) {
 
-     imageAnalysis.push({numberOfPeople: Object.keys(faces).length})
+    if (face.joyLikelihood === "VERY_LIKELY") {
+      numberOfHappyPeople += 1
+    }
 
-      var numberOfHappyPeople = 0
-      var numberOfSadPeople = 0
-      var numberOfSurprisedPeople = 0
-      var numberOfAngeredPeople = 0
-      var facesProcessed = 0;
+    if (face.sorrowLikelihood === "VERY_LIKELY") {
+      numberOfSadPeople += 1
+    }
 
-      faces.forEach(function(face) {
+    if (face.surpriseLikelihood === "VERY_LIKELY") {
+      numberOfSurprisedPeople += 1
+    }
 
-        if (face.joyLikelihood === "VERY_LIKELY") {
-          numberOfHappyPeople += 1
-        }
+    if (face.angerLikelihood === "VERY_LIKELY") {
+      numberOfAngeredPeople += 1
+    }
 
-        if (face.sorrowLikelihood === "VERY_LIKELY") {
-          numberOfSadPeople += 1
-        }
-
-        if (face.surpriseLikelihood === "VERY_LIKELY") {
-          numberOfSurprisedPeople += 1
-        }
-
-        if (face.angerLikelihood === "VERY_LIKELY") {
-          numberOfAngeredPeople += 1
-        }
-
-        facesProcessed += 1
-        if (facesProcessed === faces.length) {
-          imageAnalysis.push({numberOfHappyPeople: numberOfHappyPeople})
-          imageAnalysis.push({numberOfSadPeople: numberOfSadPeople})
-          imageAnalysis.push({numberOfSurprisedPeople: numberOfSurprisedPeople})
-          imageAnalysis.push({numberOfAngeredPeople: numberOfAngeredPeople})
-        }
-      })
+  facesProcessed += 1
+  if (facesProcessed === faces.length) {
+    imageAnalysis.push({numberOfHappyPeople: numberOfHappyPeople})
+      imageAnalysis.push({numberOfSadPeople: numberOfSadPeople})
+      imageAnalysis.push({numberOfSurprisedPeople: numberOfSurprisedPeople})
+      imageAnalysis.push({numberOfAngeredPeople: numberOfAngeredPeople})
+      }
     })
+  })
     .catch(function(err) {
       console.error('ERROR:', err)
     })
@@ -98,14 +88,7 @@ function GoogleCloudAnalysis (sourceImage, callback) {
       callback(imageAnalysis)
     })
 
-  }
-  )
-
-
-
-
-
-  client.landmarkDetection(sourceImage)
+  const result3 = await client.landmarkDetection(sourceImage)
   .then(function(results) {
    if (results[0].landmarkAnnotations[0]){
      imageAnalysis.push({nameOfLocation: results[0].landmarkAnnotations[0].description})
@@ -115,7 +98,7 @@ function GoogleCloudAnalysis (sourceImage, callback) {
     console.error('ERROR:', err)
   })
 
-  client.logoDetection(sourceImage)
+  const result4 = await client.logoDetection(sourceImage)
   .then(function(results) {
    if (results[0].logoAnnotations[0]){
      imageAnalysis.push({nameOfLogo: results[0].logoAnnotations[0].description})
@@ -126,7 +109,7 @@ function GoogleCloudAnalysis (sourceImage, callback) {
   })
 
 //WIP vvv
-  client.documentTextDetection(sourceImage)
+  const result5 = await client.documentTextDetection(sourceImage)
   .then(function(results) {
    if (results[0]){
 
@@ -136,7 +119,7 @@ function GoogleCloudAnalysis (sourceImage, callback) {
     console.error('ERROR:', err)
   })
 
-  client.imageProperties(sourceImage)
+  const result6 = await client.imageProperties(sourceImage)
   .then(function(results) {
    if (results[0]){
   //   console.log(results)
@@ -146,5 +129,6 @@ function GoogleCloudAnalysis (sourceImage, callback) {
     console.error('ERROR:', err)
   })
 
+  return imageAnalysis
 
 }
